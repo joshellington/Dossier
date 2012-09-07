cdbda7ec5d681ff76848b3b6e98046fdd9dfb7ec
Josh Ellington <hello@joshellington.com>
2012-09-07 02:41:20 -0500

    initial with links model

---
+++ app/models/link.rb
@@ -0,0 +1,3 @@
+class Link < ActiveRecord::Base
+  attr_accessible :body, :title, :url
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dossier::Application.config.secret_token = '38209c235a8fb9b219c6919ac8dfa464b01406b7fbf2c37e6d3905ba28757ddccb41bf8348d4bc59e10ada3e5937f2a9042342a3cf4f265b5b6122788782863f'

6be923138403e4f505768a2f6aa0880440a7bed1
Josh Ellington <hello@joshellington.com>
2012-11-28 15:03:29 -0800

    updated seed categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,12 @@
 
 puts 'Creating categories...'
 Category.destroy_all
-Category.create!(:id => 0, :title => 'Development')
-Category.create!(:id => 1, :title => 'Design')
-Category.create!(:id => 2, :title => 'Marketing')
-Category.create!(:id => 3, :title => 'General')
+Category.create!(:id => 0, :title => 'Technology')
+Category.create!(:id => 1, :title => 'Social')
+Category.create!(:id => 2, :title => 'News')
+Category.create!(:id => 3, :title => 'Brands')
+Category.create!(:id => 4, :title => 'Science')
+Category.create!(:id => 5, :title => 'Music')
+Category.create!(:id => 6, :title => 'Strategy')
+Category.create!(:id => 5, :title => 'Other')
 puts 'Done.'
